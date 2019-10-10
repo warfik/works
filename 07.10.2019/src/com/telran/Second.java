dfb15bdf914e4a23b75d167c661ec85030054d63
warfik <45880161+warfik@users.noreply.github.com>
2019-10-10 15:18:02 +0200

    upd second.java

--- 07.10.2019/src/com/telran/Second.java
+++ 07.10.2019/src/com/telran/Second.java
@@ -3,10 +3,11 @@ package com.telran;
 public class Second {
 
     public static void main(String[] args) {
-        int i = 100;
-        while (i > 0) {
-            System.out.println("i= " + i);
-            i = i - 10;
+        int i = 1;
+
+        while (i != 11) {
+            System.out.println ("4 * " + i + " = " + 4 * i + " ");
+            i++;
         }
     }
 }
