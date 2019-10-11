5c0c4cff3d38d3ef192c292c432ffc78c806065a
warfik <45880161+warfik@users.noreply.github.com>
2019-10-11 06:45:39 +0200

    Create Fifth
    подсчет суммы чисел с интервалом в 3, с 5 до 86

---
+++ 07.10.2019/src/com/telran/Fifth.java
@@ -0,0 +1,15 @@
+package com.telran;
+
+public class Fifth {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int sum = 0;
+        while (a <= 86) {
+            sum = sum + a;
+            a = a + 3;
+        }
+        System.out.println(sum);
+
+    }
+}
