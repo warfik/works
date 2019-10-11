5cbd3e47429cdad790cf94480a1e9cf8620aa6a5
warfik <45880161+warfik@users.noreply.github.com>
2019-10-11 05:59:30 +0200

    Create Third.java

---
+++ 07.10.2019/src/com/telran/Third.java
@@ -0,0 +1,18 @@
+package com.telran;
+
+public class Third {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 1;
+        while (a != 11) {
+            while (b != 11) {
+                System.out.print(a + " * " + b + " = " +  a * b + "; ");
+                b++;
+            }
+            System.out.println();
+            a++;
+            b = 1;
+        }
+    }
+}
