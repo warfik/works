2a0d883296faeb154b7a141827560e8afb388fa2
warfik <45880161+warfik@users.noreply.github.com>
2019-10-11 10:05:25 +0200

    create Sixth.java

---
+++ 07.10.2019/src/com/telran/Sixth.java
@@ -0,0 +1,19 @@
+package com.telran;
+
+public class Sixth {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int c = 13;
+
+        int i = 1;
+        final int x = 1000;
+        while (i <= x) {
+            if (i%a==0 || i%b==0 || i%c==0) {
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
