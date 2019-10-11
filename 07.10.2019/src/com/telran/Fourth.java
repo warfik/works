fb16b8aed50aae69b559e58ab3e0997600b7185c
warfik <45880161+warfik@users.noreply.github.com>
2019-10-11 06:37:46 +0200

    Create Fourth

---
+++ 07.10.2019/src/com/telran/Fourth.java
@@ -0,0 +1,15 @@
+package com.telran;
+
+public class Fourth {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int sum = 0;
+        while (a <= 87) {
+            sum = sum + a;
+            a++;
+        }
+        System.out.println(sum);
+
+    }
+}
