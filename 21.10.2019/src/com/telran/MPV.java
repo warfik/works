e26c2dcd3d201022ee387b60298eba69341b12e3
warfik <45880161+warfik@users.noreply.github.com>
2019-10-25 07:37:27 +0200

    add MPV

--- 21.10.2019/src/com/telran/MPV.java
+++ 21.10.2019/src/com/telran/MPV.java
@@ -3,12 +3,38 @@ package com.telran;
 public class MPV {
 
     public static void main(String[] args) {
-        int sumM;
-        int month;
-        for (int m = 50; m <= 68; m++) {
+        int sumM = 0;
+        int sumP = 0;
+        int sumV = 0;
+        int month =18;
+        int add = month-10;
+        int SalaryMV =50;
+        int SalaryP=40;
+        int monthM=SalaryMV+month;
+        int monthP = SalaryP+(month/2);
+        int monthV = SalaryMV-10;
+
+        for (int m = SalaryMV; m < monthM; m++) {
             sumM += m;
         }
+        for (int p = SalaryP; p < monthP; p++) {
+            sumP += p;
+        }
+        sumP=sumP*2;
+        for (int v=SalaryMV; v >monthV; v--){
+            sumV +=v;
+        }
+        sumV += (SalaryMV*add);
+
+        int total = sumM+sumP+sumV;
+
         System.out.println(sumM);
+        System.out.println(sumP);
+        System.out.println(sumV);
+
+        System.out.println(total);
+
+
 
     }
 
