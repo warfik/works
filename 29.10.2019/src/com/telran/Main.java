c8a8dbbd90da88d7fbf3528cd8b2376e4bfe0722
warfik <45880161+warfik@users.noreply.github.com>
2019-10-31 02:43:06 +0100

    upd. 29.10.2019

--- 29.10.2019/src/com/telran/Main.java
+++ 29.10.2019/src/com/telran/Main.java
@@ -8,7 +8,8 @@ public class Main {
         int[] array2 = fillArray(20);
         array2 = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         printArray(array2);
-        }
+    }
+
     static int[] fillArray(int length) {
         int[] res = new int[length];
         for (int i = 0; i < length; i++) {
@@ -18,12 +19,38 @@ public class Main {
         }
         return res;
     }
+
     static void printArray(int[] array) {
         System.out.println();
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
     }
+
+    //2.есть массив. сложить все нечетные числа из него.
+
+    int mass1[];
+    mass1 =new int[]{3, 5, 9, 10, 11, 14} ;
+    int sum = 0;
+    int n = 0;
+
+    for(
+    int x = 0;
+    x<mass1.length;x++)
+
+    {
+        if (mass1[x] % 2 - 1 == 0) {
+            n++;
+            sum += mass1[x];
+
+            System.out.println("Нечетный элемент = " + mass1[x]);
+        }
+    }
+
+    System.out.println("Всего нечетных элементов = "+n );
+    System.out.println("Cумма нечетных элементов = "+sum );
+}
+
 
 }
 //1.(бул)есть массив. определить есть ли в нем нечетные числа.
