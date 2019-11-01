8a7eb89f5aee85778dad6a5e7a64e3d8fa48295a
warfik <45880161+warfik@users.noreply.github.com>
2019-11-01 02:06:10 +0100

    upd. fibonachi

--- 29.10.2019/src/com/telran/Fibonachi.java
+++ 29.10.2019/src/com/telran/Fibonachi.java
@@ -1,7 +1,12 @@
 package com.telran;
 
 public class Fibonachi {
+
     public static void main(String[] args) {
+        fib();
+    }
+
+    public static void fib() {
         int[] Array = new int[20];
         for (int i = 0; i < Array.length; i++) {
             if (i < 2) {
